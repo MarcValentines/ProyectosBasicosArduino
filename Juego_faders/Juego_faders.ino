0b78b8be1e6d9f02283edefe466cea1898e368c1
MarcValentines <marcvalentinesmiro@gmail.com>
2025-05-19 10:00:48 +0200

    base de la comprobación (copiada de latas y para modificar)

--- Juego_faders/Juego_faders.ino
+++ Juego_faders/Juego_faders.ino
@@ -99,7 +99,11 @@ void loop() {
 
   if (gameRunning) {
     //lógica juego
-    
+    if (check_card(0) && check_card(1) && check_card(2) && check_card(3) && check_card(4) && check_card(5)) { //comprobación similar (modificar)
+      Serial.print("game DONE!!!");
+      sendresult(1);
+      gameRunning = false;
+    }
   }
 
 
