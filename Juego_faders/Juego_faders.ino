5edf1953c5408a9adfe1aa8f87f2c32ac83eca8f
MarcValentines <marcvalentinesmiro@gmail.com>
2025-05-19 10:36:50 +0200

    Método bool para verificar máximos y mínimos

--- Juego_faders/Juego_faders.ino
+++ Juego_faders/Juego_faders.ino
@@ -11,12 +11,7 @@
 
 //definición de los pines de los faders´
 #define CANT_FADERS 6
-#define FADER_1 A0
-#define FADER_2 A1
-#define FADER_3 A2
-#define FADER_4 A3
-#define FADER_5 A4
-#define FADER_6 A5
+const int potPins[CANT_FADERS] = {A0, A1, A2, A3, A4, A5};
 //rangos
 const unsigned int rangoMin[CANT_FADERS] = {500, 200, 350, 100, 600, 400};
 const unsigned int rangoMax[CANT_FADERS] = {530, 230, 380, 130, 630, 430};
@@ -108,10 +103,21 @@ void loop() {
       sendresult(1);
       gameRunning = false;
     }
+  }   
+
+}
+//método para verificar si los potenciómetros se encuentran en su rango
+bool verificarFaders(){
+  for(int i = 0, i < CANT_FADERS, i++){
+    int valor = analogRead(potPins[i]);
+    Serial.print("Fader: " + (i+1));
+    Serial.print(": " + valor);
+
+    if (valor < rangoMin[i] ||valor > rangoMax[i]){
+      return false;
+    }
   }
-
-
-        
+  return true;
 }
 
 void routeStart(OSCMessage &msg, int addrOffset ){
